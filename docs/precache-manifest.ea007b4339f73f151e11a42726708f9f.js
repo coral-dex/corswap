ba529542da0bcf5e69d57c1ba457d9976f5db21c
rhinoplus <rhino.plus@protonmail.com>
2020-09-18 14:53:14 +0800

    update docs

---
+++ docs/precache-manifest.ea007b4339f73f151e11a42726708f9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fae65225cbf16b9b089b773d6c6f0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f8260102c9d20fb5a7a",
+    "url": "/static/css/2.faa665ad.chunk.css"
+  },
+  {
+    "revision": "9dadddeff9cba11e3446",
+    "url": "/static/css/main.da5ab0cd.chunk.css"
+  },
+  {
+    "revision": "9f8260102c9d20fb5a7a",
+    "url": "/static/js/2.ccc47180.chunk.js"
+  },
+  {
+    "revision": "928834ae7e551df949d1be444f0ac226",
+    "url": "/static/js/2.ccc47180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dadddeff9cba11e3446",
+    "url": "/static/js/main.01175608.chunk.js"
+  },
+  {
+    "revision": "0b1504a8c1b17ae6cae6",
+    "url": "/static/js/runtime-main.055d6c94.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52073b96e6b4c27dfdf6b0127f1a3130.js"
+  "/precache-manifest.ea007b4339f73f151e11a42726708f9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
